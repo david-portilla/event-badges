985df02c33c30580beab4345bb81b736497e7857
David Portilla <david_portilla@hotmail.com>
2020-01-13 10:01:16 -0500

    transform BadgeDetails into a Container Component and create a presentational component for details

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import Layout from '../components/Layout'
 import Badges from '../pages/Badges'
 import BadgeNew from '../pages/BadgeNew'
 import BadgeEdit from '../pages/BadgeEdit'
-import BadgeDetails from '../pages/BadgeDetails'
+import BadgeDetailsContainer from '../pages/BadgeDetailsContainer'
 import NotFound from '../pages/NotFound'
 import HomePage from '../pages/HomePage'
 
@@ -18,7 +18,7 @@ export default function App () {
             <Route exact path="/" component={ HomePage } />
             <Route exact path="/badges" component={ Badges } />
             <Route exact path="/badges/new" component={ BadgeNew } />
-            <Route exact path="/badges/:badgeId" component={ BadgeDetails} />
+            <Route exact path="/badges/:badgeId" component={ BadgeDetailsContainer} />
             <Route exact path="/badges/:badgeId/edit" component={ BadgeEdit } />
             <Route component={ NotFound } />
           </Switch>
